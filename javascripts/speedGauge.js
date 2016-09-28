f6f968739080e67eed3f6b6335511b4ad9ff0772
Jacques Jordan <jjordansd@users.noreply.github.com>
2016-09-28 14:54:41 -0700

    Add files via upload
    SpeedGauge.js

---
+++ javascripts/speedGauge.js
@@ -0,0 +1,119 @@
+window.speedometerGauge=0;
+freeboard.addStyle('.speedgauge-widget-wrapper', "color:#0000000;width:75%;	height:240px;margin-left:35px");
+
+(function() {    
+    var speedometerGaugeWidget = function (settings) {
+        var self = this;
+        var currentSettings = settings;
+        var thisgaugeID = "speedgauge-" + window.speedometerGauge++;
+        var titleElement = $('<h2 class="section-title"></h2>');
+        var speedgaugeElement = $('<div class="speedgauge-widget-wrapper" id="' + thisgaugeID + '"></div>');
+        var gaugeChart = null;
+        
+        this.render = function(element) {
+            
+            $(element).append(titleElement).append(speedgaugeElement);
+            gaugeChart = AmCharts.makeChart( thisgaugeID, {
+                "type": "gauge",
+                "theme": "dark",
+                "fontSize": 9,
+                "path": "http://www.amcharts.com/lib/3/",
+                  "axes": [ {
+                    "axisThickness": 1,
+                    "axisAlpha": 0.2,
+                    "tickAlpha": 0.2,
+                    "valueInterval": 20,
+                    "bands": [ {
+                      "color": "#84b761",
+                      "innerRadius": "98%",
+                      "endValue": 90,
+                      "startValue": 0
+                    }, {
+                      "color": "#fdd400",
+                      "innerRadius": "98%",
+                      "endValue": 130,
+                      "startValue": 90
+                    }, {
+                      "color": "#cc4748",
+                      "endValue": 220,
+                      "innerRadius": "98%",
+                      "startValue": 130
+                    } ],
+                    "bottomText": "0 km/h",
+                    "bottomTextYOffset": -20,
+                    "endValue": 220
+                  } ],
+                    "arrows": [ {
+                        "innerRadius": "3%",			
+                        "radius": "95%",
+        			    "startWidth": 5
+        		    } ],
+                  "export": {
+                    "enabled": true
+                  }
+            });
+            freeboard.showLoadingIndicator(true);
+            setTimeout(function(){
+                freeboard.showLoadingIndicator(false);
+                speedgaugeElement.find("a").css("display","none");
+                titleElement.html("Vehicle Speed");
+            },10000);
+            
+        }
+        this.getHeight = function () {
+            return 5;
+        }
+        this.onSettingsChanged = function(newSettings)
+        {
+            currentSettings = newSettings;
+            titleElement.html((_.isUndefined(newSettings.title) ? "" : newSettings.title));
+
+        }
+        this.onCalculatedValueChanged = function(settingName, newValue)
+		{
+		    if (gaugeChart) {
+		        if ( gaugeChart.arrows ) {
+                    if ( gaugeChart.arrows[ 0 ] ) {
+                        if ( gaugeChart.arrows[ 0 ].setValue ) {
+                            gaugeChart.arrows[ 0 ].setValue( newValue );
+                            gaugeChart.axes[ 0 ].setBottomText( newValue + " MPH" );
+                        }
+                    }
+                }
+		    }
+        }
+        this.onDispose = function() 
+        {
+        
+        }
+
+        
+    };
+        
+        
+    freeboard.loadWidgetPlugin({
+        type_name: "speedgauge",
+        display_name: "SpeedometerGauge",
+        "external_scripts" : [
+            "https://www.amcharts.com/lib/3/amcharts.js",
+            "https://www.amcharts.com/lib/3/gauge.js",
+            "https://www.amcharts.com/lib/3/themes/dark.js"
+        ],
+        settings: [
+            {
+                name: "title",
+                display_name: "Title",
+                type: "text"
+            },
+            {
+                name: "value",
+                display_name: "Value",
+                type: "calculated"
+            }        
+        ],
+        newInstance: function(settings, newInstanceCallback) {
+            newInstanceCallback(new speedometerGaugeWidget(settings));
+        }
+    
+    });
+}());
